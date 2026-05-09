82afdfe2c4be0603ab838b47447f48c1468785a7
Jude Armah <judearmah95@gmail.com>
2026-05-09 16:05:06 +0000

    JavaScript W1

---
+++ myscript.js
@@ -0,0 +1,30 @@
+// strings
+let a= "Jude"
+let b= "Kofi"
+let c= "Ama"
+let d= "kwame"
+let e= "Kobby"
+
+
+// Int
+let f= 1
+let g= 2
+let h= 3
+let i= 4
+let j= 5
+
+// Float
+let k= 1.5
+let l= 2.5
+let m= 3.5
+let n= 4.5
+let o= 5.5
+
+// Boolean
+let p= true
+let q= false
+let r= true
+let s= false
+let t= true
+
+console.log(a, b, c, d, e, f, g, h, i, j)
